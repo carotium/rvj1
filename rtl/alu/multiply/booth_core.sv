af90f8242b41f40937511a3d1cb3a69c72bcec90
carotium <coolboy9987@gmail.com>
2026-09-22 13:37:49 +0200

    Added sign extended signal for assigning to partial products for correct calculation of unsigned and signed multiplication.

--- rtl/alu/multiply/booth_core.sv
+++ rtl/alu/multiply/booth_core.sv
@@ -6,6 +6,8 @@ module booth_core #(parameter int XLEN = 32) (
 
   logic [15:0] signs, ones, twos;
   logic [XLEN*2-1:0] pps [15:0];
+  logic [XLEN*2-1:0] op_a_2XLEN_sign_ext;
+  logic [XLEN*2-1:0] op_b_2XLEN_sign_ext;
 
   genvar i;
   generate
@@ -16,6 +18,8 @@ module booth_core #(parameter int XLEN = 32) (
       end else assign bth = op_b_ext[2*i+1:2*i-1];
       encoder booth_encoder(.bth(bth), .sign(signs[i]), .one(ones[i]), .two(twos[i]));
 
+      assign op_a_2XLEN_sign_ext = (op_a_ext[XLEN]) ? {{(XLEN*2 - XLEN){1'b1}}, op_a_ext} : {{(XLEN*2 - XLEN){1'b0}}, op_a_ext};
+      assign op_b_2XLEN_sign_ext = (op_b_ext[XLEN]) ? {{(XLEN*2 - XLEN){1'b1}}, op_b_ext} : {{(XLEN*2 - XLEN){1'b0}}, op_b_ext};
       // Set partial products to booth encoded multiplier * multiplier
       // and << 2 for every consecutive PP.
       // 2*Q  = M << 1
@@ -23,11 +27,10 @@ module booth_core #(parameter int XLEN = 32) (
       // 0*Q  = 0
       // -1*Q = NOT(M)
       // -2*Q = NOT(M << 1)
-      assign pps[i][XLEN*2-1:2*i] = (ones[i] & signs[i]) ? ~op_a_ext :
-	                            (twos[i] & signs[i]) ? ~(op_a_ext << 1) :
-		                    //(ones[i])            ? {{(XLEN*2-1-2*i){op_a_ext[XLEN]}}, op_a_ext}         :
-		                    (ones[i])            ? op_a_ext         :
-		                    (twos[i])            ? (op_a_ext << 1)  :
+      assign pps[i][XLEN*2-1:2*i] = (ones[i] & signs[i]) ? ~op_a_2XLEN_sign_ext        :
+	                            (twos[i] & signs[i]) ? ~(op_a_2XLEN_sign_ext << 1) :
+		                    (ones[i])            ? op_a_2XLEN_sign_ext         :
+		                    (twos[i])            ? (op_a_2XLEN_sign_ext << 1)  :
 		                                           'b0;
       // Negative correction for -1*Q and -2*Q at
       assign pps[i+1][2*i] = (signs[i] & (ones[i] | twos[i]) & (i < 15)) ? 1'b1 : 1'b0;
