2dff595b2f699756fa9fad3536e7000238e19181
carotium <coolboy9987@gmail.com>
2026-09-24 13:14:13 +0200

    Added clock connected to alu unit.

--- rtl/rvj1_top.sv
+++ rtl/rvj1_top.sv
@@ -267,6 +267,7 @@ module rvj1_top import rvj1_pkg::*; #(
   assign alu_op_b_data = rpb_or_imm ? immediate       : regs2_data;
 
   rvj1_alu alu_inst(
+    .clk_i  (clk_i),
     .sel_i  (alu_op_sel),
     .op_a_i (alu_op_a_data),
     .op_b_i (alu_op_b_data),
